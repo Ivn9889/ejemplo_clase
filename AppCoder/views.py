c80947898d9255674e1da9d33637170e636e575f
Ivan <ivannnestor98@hotmail.com>
2025-05-10 12:30:09 -0300

    se generó inicio

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render, get_object_or_404
 # y coloca las variables que le pasamos. El get_object_or_404 es para obtener un objeto o 
 # devolver un 404 si no existe.
 from .models import Estudiante, Profesor, Entregable, Curso
-
+def index(request):
+    return render(request, 'AppCoder/index.html')
 def lista_estudiantes(request):
     estudiantes = Estudiante.objects.all() #trae todos los estudiantes de la base de datos
     # y los guarda en la variable estudiantes. El objects.all() es un método de Django 
@@ -16,5 +17,11 @@ def detalle_estudiante(request,pk):
     estudiante = get_object_or_404(Estudiante, pk=pk) #busca el estudiante por pk, 
     # si no lo encuentra devuelve 404. Donde el pk es el id del estudiante.
     return render(request, 'AppCoder/estudiantes_detail.html', {'estudiante': estudiante})
-
+def lista_profesores(request):
+    profesores = Profesor.objects.all() #trae todos los profesores de la base de datos
+    # y los guarda en la variable profesores. El objects.all() es un método de Django 
+    # que trae todos los objetos de una tabla
+    contexto = {'profesores': profesores} #creamos un diccionario con la variable profesores
+    # y la pasamos al template.
+    return render(request, 'AppCoder/profesores.html', contexto)
 # Create your views here.
