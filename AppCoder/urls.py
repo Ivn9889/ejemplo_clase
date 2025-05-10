f6d7565ab9d14fba93ebf9c4ceb15499111d00b5
Ivan <ivannnestor98@hotmail.com>
2025-05-10 12:44:14 -0300

    se generó inicio

--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import lista_estudiantes, detalle_estudiante, lista_profesores, index
 app_name = 'AppCoder'
 urlpatterns = [
+    path('', index, name='index'),
     path('estudiantes/', lista_estudiantes, name='lista_estudiantes'),
     path('estudiantes/<int:pk>/', detalle_estudiante, name='detalle_estudiante'),
-    path('profesores/', lista_profesores, name='lista_profesores'),
-    path('', index, name='index'),
+    path('profesores/', lista_profesores, name='lista_profesores')
 ]
